fc24053c9927edc17578b36bfcfe233b52d5afbf
Kenneth Reitz <me@kennethreitz.com>
2007-07-04 05:30:18 +0000

    unecessary files

--- tantan/wordpress-s3/js/wiki-config.js
+++
@@ -1,9 +0,0 @@
-/**
-    S3AjaxWiki site-specific configuration
-*/
-with (S3AjaxWiki) {
-    DEBUG       = true;
-    ANONYMOUS   = true;
-    BUCKET      = 's3wiki';
-    DEFAULT_ACL = 'public-read-write';
-}
